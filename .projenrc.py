54c9aba24d1c094cc36be26632a499736cba7a14
anrich <anrich@labs.epiuse.com>
2023-08-09 14:03:22 +0200

    Add link to repository on Poetry, update description.

--- .projenrc.py
+++ .projenrc.py
@@ -2,6 +2,7 @@ from projen.python import PythonProject
 
 project = PythonProject(
     name="DioMindmap",
+    description="Create Draw.io mind map diagrams with indented text files.",
     module_name="diomindmap",
     version="0.1.0",
     author_email="anrichvs@gmail.com",
@@ -10,7 +11,10 @@ project = PythonProject(
           "N2G@0.3.3",
           "python-igraph@0.10.6"],
     pytest=False,
-    poetry=True
+    poetry=True,
+    poetry_options={
+        "repository": "https://github.com/AnrichVS/diomindmap"
+    }
 )
 project.add_git_ignore(".idea/")
 project.synth()

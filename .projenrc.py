1ff3b483c5218bf81636453eeb8351f43838bc6a
anrich <anrich@labs.epiuse.com>
2023-08-09 13:52:14 +0200

    Update email address for PyPI publishing

--- .projenrc.py
+++ .projenrc.py
@@ -4,7 +4,7 @@ project = PythonProject(
     name="DioMindmap",
     module_name="diomindmap",
     version="0.1.0",
-    author_email="@anrichvs",
+    author_email="anrichvs@gmail.com",
     author_name="Anrich van Schalkwyk",
     deps=["typer@0.9.0",
           "N2G@0.3.3",
